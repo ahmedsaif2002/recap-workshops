251022379472724b120705bd1e6ec027d83d4ae9
ahmedsaif2002 <ahmedsaif2002@gmail.com>
2023-03-14 18:10:26 +0000

    recap-workshops

--- debugging/2.js
+++ debugging/2.js
@@ -5,14 +5,22 @@ Can you fix the bug in this function
 */
 
 function getLargestNum(arr) {
-    let largestNum = 0;
-    for (let i = 0; i <= arr.length; i++) {
-      if (arr[i] > largestNum) {
-        largestNum = arr[i];
-      }
-    }
-    return largestNum;
-  }
+    
+        let largestNum = arr[0]; 
+
+        for (let i = 0; i < arr.length; i++) {
+        	if (arr[i] > largestNum) 
+          console.log(i);
+        		largestNum = arr[i]; 
+        } 
+        return largestNum; 
+        
+      
+    } 
+        
+    
+  
   let arr = [1, 2, 3, 4, 5];
 
-  getLargestNum(arr)
+console.log(getLargestNum(arr + ''));
+ 
--- debugging/4.js
+++ debugging/4.js
@@ -6,11 +6,13 @@ Can you spot why this function is not working and resolve.
 function calculateSum(arr) {
     let sum = 0;
     for (let i = 0; i <= arr.length; i++) {
-      sum += arr[i];
+      sum += +(parseInt(arr[i],5));
     }
     return sum;
+    // console.log(sum);
   }
   
   let arr = [1, 2, 3, 4, 5];
   let sum = calculateSum(arr);
+  //  console.log(calculateSum(arr)); 
   console.log("The sum of " + arr + " is " + sum);
